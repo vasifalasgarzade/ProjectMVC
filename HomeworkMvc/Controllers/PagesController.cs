d4478708f1416ceed01460e8b58bbb03bb358b9a
vasifalasgarzade <vasiffa@code.edu.az>
2019-11-25 14:01:04 -0800

    add_new

---
+++ HomeworkMvc/Controllers/PagesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeworkMvc.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Page
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Team()
+        {
+            return View();
+        }
+        public ActionResult Faqs()
+        {
+            return View();
+        }
+        public ActionResult PricingPlan()
+        {
+            return View();
+        }
+      
+    }
+}
